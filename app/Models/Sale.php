5b56cefe14e65d185197f632ac0203d8db40c15e
kevincisnero01 <kevingabriel18@gmail.com>
2023-10-20 12:11:46 -0400

    feat(sale): add database files

---
+++ app/Models/Sale.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sale extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'user_id',
+        'total',
+        'status',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function customer(){
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function saleDetails(){
+        return $this->hasMany(saleDetail::class);
+    }
+}
